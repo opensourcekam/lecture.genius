ce82875212909d685d58a71e569b9c79beec9f5b
bp70291 <bp70291@umbc.edu>
2017-04-08 13:19:17 -0400

    Database route connections

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,108 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.put('/edit/:lecture', (req, res, next) => {
+	// body...
+		/* 
+	database.findAndUpdate(lectureID, (data) => {
+		console.log('COMPLETE')
+	})
+	*/
+})
+
+router.post('/delete/:lecture', (req, res, next) => {
+	let lectureID = req.params.lecture;
+	
+	/* 
+	database.findAndDelete(lectureID, (data) => {
+		console.log('COMPLETE')
+	})
+	*/
+})
+
+router.post('/create', (req, res, next) => {
+	// use query params
+	// let lecture = new Lecture({
+	// 	course_title: req.query.name,
+	// 	course_date: req.query.date,
+	// 	course_instructor: req.query.instructor
+	// });
+
+	// database.create('Lecture', lecture)
+})
+
+router.post('/search/:lecture', (req, res, next) => {
+	// body...
+})
+
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      db.close();
+    });
+  });
+});
+
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      db.close();
+    });
+  });
+});
+
 module.exports = router;
