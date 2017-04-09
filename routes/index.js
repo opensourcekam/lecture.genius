7ed509b4dad06318055dfdb5ddafc8c30fea217e
bp70291 <bp70291@umbc.edu>
2017-04-09 08:06:04 -0400

    Add username and password authentication

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,36 @@
-var Class = require('../database/models/Class.js');
-var Lecture = require('../database/models/Lecture.js');
-var express = require('express');
-var router = express.Router();
-var db = require('../connection.js');
+const Class = require('../database/models/Class.js');
+const Lecture = require('../database/models/Lecture.js');
+const express = require('express');
+const router = express.Router();
+const db = require('../connection.js');
 
 
-/*
 
-  CLASSES
-    POST method to create a new class
-      /create/class
-      {
-        ...
-        lectures: [lectureIds]
-      }
-  LECTURES
-    POST method to create a lecture
-    /create/lecture/
-      POST body contains class id
-
-      lecture object in database ~ {
-        ....
-        classId: 
-      }
-
-
-*/
+// //Landing page
+// router.get('/'. function(req, res, next) {
+//   res.render
+// });
 
 router.post('/create/class', (req, res, next) => {
-	// use query params
-  console.log(req.body, req.query, req.params);
-  if (!req.body._id) {
+    // use query params
+    console.log(req.body, req.query, req.params);
+    if (!req.body._id) {
         res.status(400);
-  }
-
-  Class.create(req.body, (err, doc) => {
-    console.log("Reach Arrow function of Class.create");
-    console.log(doc);
-    if(err){
-      res.json({"error": err});
-    } else {
-      res.json({"success": doc})
     }
- });
+
+    Class.create(req.body, (err, doc) => {
+        console.log("Reach Arrow function of Class.create");
+        console.log(doc);
+        if (err) {
+            res.json({
+                "error": err
+            });
+        } else {
+            res.json({
+                "success": doc
+            })
+        }
+    });
 })
 
 router.post('/create/lecture', (req, res, next) => {
@@ -53,61 +41,70 @@ router.post('/create/lecture', (req, res, next) => {
     // lectures needs to match the _class with the _id of the Class model
     // so here we need to grab out the id for our class
     Class.findById(req.body._id, (err, classDoc) => {
-      console.log('req.body', req.body)
-      let { title, lectureNotes, courseDate, courseInstructor } = req.body;
-      let lecture = Lecture.create({
-        _creator: req.body._id,
-        title,
-        lectureNotes,
-        courseDate,
-        courseInstructor
-      }, (err, lectureDoc) => {
-          console.log(lectureDoc);
-          classDoc.lectures.push(lectureDoc);
-          classDoc.save((err, doc) => {
-            if(err){
-              res.json(err);
-            } else {
-              res.json(doc)
-            }
-          });
-      })
+        console.log('req.body', req.body)
+        let {
+            title,
+            lectureNotes,
+            courseDate,
+            courseInstructor
+        } = req.body;
+        let lecture = Lecture.create({
+            _creator: req.body._id,
+            title,
+            lectureNotes,
+            courseDate,
+            courseInstructor
+        }, (err, lectureDoc) => {
+            console.log(lectureDoc);
+            classDoc.lectures.push(lectureDoc);
+            classDoc.save((err, doc) => {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(doc)
+                }
+            });
+        })
 
     })
 
 })
 
 router.post('/search/:id', (req, res, next) => {
-  Class.findById(res.query.id, (err, doc) => {
-    console.log(doc);
-  })
+    Class.findById(res.query.id, (err, doc) => {
+        console.log(doc);
+    })
 })
 
 router.put('/edit', function(req, res) {
-  var id = req.body.id;
-  console.log(req.body)
-  Class.update(req.body.id, function (err, doc) {
+    var id = req.body.id;
+    console.log(req.body)
+    Class.update(req.body.id, function(err, doc) {
         console.log(doc)
         res.send(
-            (err === null) ? {msg: ''} : {msg: err}
+            (err === null) ? {
+                msg: ''
+            } : {
+                msg: err
+            }
         );
     });
 });
 
 router.delete('/delete', function(req, res, next) {
-  var id = req.body.id;
-  console.log(req.body)
-  Class.findByIdAndRemove(req.body.id, function(err, r) {
-    res.json(r)
-  });
+    var id = req.body.id;
+    console.log(req.body)
+    Class.findByIdAndRemove(req.body.id, function(err, r) {
+        res.json(r)
+    });
 });
 
 router.get('/classes', (req, res, next) => {
-  Class.find({}).populate('lectures').exec(function (err, doc) {
-  // console.log(doc)
-  res.json(doc)
-  // console.log(doc.populated('author')) // '5144cf8050f071d979c118a7'
-})
+    Class.find({}).populate('lectures').exec(function(err, doc) {
+        // console.log(doc)
+        res.json(doc)
+        // console.log(doc.populated('author')) // '5144cf8050f071d979c118a7'
+    })
 })
 
 module.exports = router;
---
+++ routes/student.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var Student = require('../database/Models/Student.js')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/login', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  Student.findOne({username: username, password: password}, function(err, student) {
+    if(err) {
+      console.log(err);
+      return res.status(500).send();
+    }
+
+  if(!user) {
+    return res.status(404).send();
+  }
+
+  return res.status(200).send()
+
+  });
+
+router.post('/register', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+
+  var newstudent = new Student();
+  newstudent.username = username;
+  newstudent.password = password;
+  newstudent.firstname = firstname;
+  newstudent.lastname = lastname;
+  newstudent.save(function(err, savedStudent) {
+    if(err) {
+        console.log(err)
+        return res.status(500).send();
+    }
+        return res.status(200).json({savedStudent});
+    })
+
+ })
+
+module.exports = router;
+
+ })
